5632346f78ac18704c3917ebe99806848620e675
eduardo.souza <eduardo.souza@benner.com.br>
2016-06-18 11:20:08 -0300

    no message

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var sout = "HelloWorld";
+            var scout = "HelloWorld";
             Console.WriteLine(sout);
             Console.ReadKey();
         }
